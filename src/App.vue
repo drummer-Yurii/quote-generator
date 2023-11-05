a5a049010322bba3bac684670d686ea0579c7d2e
Yurii Rybachok <drummer2009@ukr.net>
2023-11-05 11:14:09 +0200

    add basic styles

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,19 @@ export default {
 </script>
 
 <style lang="scss">
+:root {
+  --primary: #D81E5B;
+  --secondary: #8A4FFF;
+  --tertiary: #32CBFF;
+  --dark: #131A26;
+  --light: #EEE;
+  --grey: #848484;
+}
 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
 </style>
